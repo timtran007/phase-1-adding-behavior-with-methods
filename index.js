ed9f08ccd8289b04047de8912f7ea2f174623870
Tim <timtran007@gmail.com>
2023-03-18 23:19:39 -0500

    commit tree 75b70035434d0d74e05c4de6f2ad6f01d6664c34

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+// Create 3 classes: Cat, Dog and Bird with parameters naem and sex to be stored as value
+
+// For an instance of Cat, speak returns "name says meow!",
+// For an instance of Dog, speak returns "name says woof!"
+// For an instance of Bird, speak returns conditional output. If the instance of Bird is male, speak returns "It's me! name, the parrot!". If it is not male, speak returns "name says squawk!".
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        if(this.sex === "male"){
+            return `It's me! ${this.name}, the parrot!`
+        } else{
+            return `${this.name} says squawk!`
+        }
+    }
+}
